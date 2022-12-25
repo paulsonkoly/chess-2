36648b2e3697f8af0615484af14806029453f3d8
Paul Sonkoly <sonkoly.pal@gmail.com>
2022-12-25 12:01:20 +0000

    oops fix CI

--- test/zobrist_tests.c
+++ test/zobrist_tests.c
@@ -19,7 +19,7 @@ void zobrist_test1(void **state) {
   board = parse_fen("k7/4p3/8/5P2/8/8/8/4K2R w K - 0 1");
   play_uci_moves(board, "e1g1 e7e5 f5e6 a8a7 e6e7 a7a6 e7e8q");
 
-  assert_int_equal(board->history[board->halfmovecnt], calculate_hash(board));
+  assert_int_equal(board->history[board->halfmovecnt].hash, calculate_hash(board));
 
   free(board);
 }
@@ -33,7 +33,7 @@ void zobrist_test2(void **state) {
   board = parse_fen("4k3/8/8/8/8/8/8/4K3 w - - 0 1");
   play_uci_moves(board, "e1e2 e8e7 e2e1 e7e8");
 
-  assert_int_equal(board->history[board->halfmovecnt], board->history[0]);
+  assert_int_equal(board->history[board->halfmovecnt].hash, board->history[0].hash);
 
   free(board);
 }

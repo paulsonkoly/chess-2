bf49ae8062fe69bb16e55483910d2714470ea216
Paul Sonkoly <sonkoly.pal@gmail.com>
2023-02-08 17:01:13 +0000

    make clean between runs, in case we want to measure a compile flag change

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -15,24 +15,26 @@ printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
 }
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
+make clean
 make
 
 echo Running chess2 bench 30x
 rm -f "${BRANCH}_bench_results.txt"
 
 for (( i = 0; i < 30; i++ )); do
-  ProgressBar $i 30
+  ProgressBar $i 29
   echo `./chess2 bench | tail -n 2 | tr '\n' ' '` >> "${BRANCH}_bench_results.txt"
 done
 echo
 
 echo Checking out main
 git checkout main
+make clean
 make
 
 rm -f "main_bench_results.txt"
 for (( i = 0; i < 30; i++ )); do
-  ProgressBar $i 30
+  ProgressBar $i 29
   echo `./chess2 bench | tail -n 2 | tr '\n' ' '` >> "main_bench_results.txt"
 done
 echo

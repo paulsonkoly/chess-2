64a3703e28796f80431b6d91d8e0c2f1a4293839
Paul Sonkoly <sonkoly.pal@gmail.com>
2023-02-10 18:21:33 +0000

    added script

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,18 +1,4 @@
-# https://stackoverflow.com/questions/238073/how-to-add-a-progress-bar-to-a-shell-script
-function ProgressBar {
-# Process data
-    let _progress=(${1}*100/${2}*100)/100
-    let _done=(${_progress}*4)/10
-    let _left=40-$_done
-# Build progressbar string lengths
-    _fill=$(printf "%${_done}s")
-    _empty=$(printf "%${_left}s")
-
-# 1.2 Build progressbar strings and print the ProgressBar line
-# 1.2.1 Output example:
-# 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
-}
+source scripts/progress.sh
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 make clean
